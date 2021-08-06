888651643f4b520ec3e8718b5537069c7c698536
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-06 22:43:56 +0200

    Initial Commit

---
+++ FullPop/FullPopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullPopApp.swift
+//  FullPop
+//
+//  Created by Michele Manniello on 06/08/21.
+//
+
+import SwiftUI
+
+@main
+struct FullPopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
